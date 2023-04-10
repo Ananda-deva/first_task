3a3897b2e75315b9fd0d44b28768651afa996ab5
Ananda-deva <babichenko.a.v@mail.ru>
2023-04-10 17:53:44 +0700

    second

--- JS/New_Project.js
+++ JS/New_Project.js
@@ -1,25 +1,14 @@
-let money = prompt('Ваш бюджет на месяц?', '')
-	time = prompt('Введите дату в формате YYYY-MM-DD', '');
+let num = 50
 
-	let appData = {
-		budget: money,
-		timeData: time,
-		expenses: {},
-		OptionalExpenses: {},
-		income: [],
-		savings: false
-	};
+if (num<49){
+	console.log('Верно')
+}	else if(num > 100){
+	console.log('Много')
+} else {
+	console.log('Верно!')
+}
 
-	let 
-	q1 = prompt('Введите обязательную статью расходов в этом месяце', ''),
-	q2 = prompt('Во сколько обойдется?', ''),
-	q3 = prompt('Введите обязательную статью расходов в этом месяце', ''),
-	q4 = prompt('Во сколько обойдется?', '');
 
-	appData.expenses.q1 = q2;
-	appData.expenses.q3 = q4;
-
-	alert(appData.budget / 30);
 
 
 
